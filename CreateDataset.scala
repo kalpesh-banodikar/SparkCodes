d506b3e463816c49a07749b9d7ee5db154ddb0d9
kalpesh-banodikar <37872440+kalpesh-banodikar@users.noreply.github.com>
2019-11-19 02:08:36 +0530

    Update CreateDataset.scala

--- CreateDataset.scala
+++ CreateDataset.scala
@@ -8,14 +8,14 @@ object CreateDataset extends App{
   val spark= SparkSession.builder().appName("Create Dataset").config("spark.master","local").getOrCreate()
   import spark.implicits._
   val caseDS=Seq(person("Andy",32)).toDS()
-  println(caseDS.show())
+  //println(caseDS.show())
 
   val primDS=Seq(1,2,3).toDS()
-  println(primDS.map(_+1).collect().foreach(println))
+  //println(primDS.map(_+1).show())
 
 
-  var peepDS=spark.read.json("/home/kalpesh/Downloads/spark1/spark-2.4.4-bin-hadoop2.7/examples/src/main/resources/people.json").as[person]
-  println(peepDS.show())
+  var peepDS=spark.read.json("/home/kalpesh/Downloads/spark1/spark-2.4.4-bin-hadoop2.7/examples/src/main/resources/people.json").rdd
+  println(peepDS.collect().foreach(println))
   val peepDF = spark.sparkContext
     .textFile("/home/kalpesh/Downloads/spark1/spark-2.4.4-bin-hadoop2.7/examples/src/main/resources/people.txt")
     .map(_.split(","))
